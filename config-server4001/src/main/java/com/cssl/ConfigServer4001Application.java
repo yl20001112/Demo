e345b0cd55259abb0a8285c71edc608796ad7344
yl20001112 <karsaking@163.com>
2020-03-06 17:25:42 +0800

    新增了config-server4001

---
+++ config-server4001/src/main/java/com/cssl/ConfigServer4001Application.java
@@ -0,0 +1,13 @@
+package com.cssl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServer4001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer4001Application.class, args);
+    }
+
+}
---
+++ config-server4001/src/test/java/com/cssl/ConfigServer4001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cssl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServer4001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
